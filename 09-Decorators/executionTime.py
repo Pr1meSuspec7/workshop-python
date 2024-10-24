da8ae1c21a49614c923daa19f1f19b9cc531df93
Pr1meSuspec7 <marco.estremo@gmail.com>
2024-10-24 14:23:27 +0200

    09-Decorators

---
+++ 09-Decorators/executionTime.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+from time import sleep
+
+#def executionTime(func):
+#    def wrapper():
+#        print('Tracking execution time...')
+#        print('')
+#        start = datetime.now()
+#        func()
+#        end = datetime.now()
+#        td = (end - start).total_seconds()
+#        print('')
+#        print(f"Execution Time : {td:.03f}s")
+#    return wrapper
+#
+#@executionTime
+
+def func1():
+    print('func1 execution...')
+    sleep(2)
+
+def func2():
+    print('func2 execution...')
+    sleep(10)
+
+def func3():
+    print('func3 execution...')
+    sleep(5)
+
+def main():
+    func1()
+    func2()
+    func3()
+
+if __name__ == '__main__':
+    main()
